8ea0fa3483ecd1407a1c1a566de5c37fd21affab
fanyangr <fanyangr@amazon.com>
2025-07-24 17:56:29 -0700

    add nccl test

---
+++ nccl_test.sh
@@ -0,0 +1,10 @@
+set -ex
+bazel build covariant/models/llm/scripts:nccl_test
+torchrun \
+    --nproc_per_node=${SKYPILOT_NUM_GPUS_PER_NODE:-`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`} \
+    --node_rank=${SKYPILOT_NODE_RANK:-0} \
+    --nnodes=${SKYPILOT_NUM_NODES:-1} \
+    --master_addr=${SKYPILOT_MASTER_ADDR:-localhost} \
+    --master_port=9009 \
+    bazel-bin/covariant/models/llm/scripts/nccl_test \
+    $@
