3f9b9a62ad508d5cbb17be95fd3b19e8915178b3
fanyangr <fanyangr@amazon.com>
2025-08-08 17:37:17 -0700

    add launch script

---
+++ launch.sh
@@ -0,0 +1,4 @@
+cd /root/sky_workdir/FAR-affordance/molmo/
+conda activate molmo
+wandb login --host=https://far.wandb.io
+PYTHONPATH=. torchrun --nproc-per-node 8 --nnodes=${SKYPILOT_NUM_NODES} --node_rank=${SKYPILOT_NODE_RANK} --rdzv_id=311 --rdzv_endpoint=${SKYPILOT_MASTER_ADDR}:29500 launch_scripts/train_affordance.py affordance_human_robot checkpoints/Molmo-7B-O-0924 --save_folder=finetuned_checkpoints --save_overwrite --wandb.name=human_robot_cotrain --wandb.entity=fanyangr --wandb.project=affordance --cotrain --use_transitions $((SKYPILOT_NUM_NODES*8*2)) --global_batch_size 
--- nccl_test.sh
+++
@@ -1,9 +0,0 @@
-set -ex
-torchrun \
-    --nproc_per_node=${SKYPILOT_NUM_GPUS_PER_NODE:-`nvidia-smi --query-gpu=name --format=csv,noheader | wc -l`} \
-    --node_rank=${SKYPILOT_NODE_RANK:-0} \
-    --nnodes=${SKYPILOT_NUM_NODES:-1} \
-    --master_addr=${SKYPILOT_MASTER_ADDR:-localhost} \
-    --master_port=9009 \
-    /root/sky_workdir/FAR-affordance/nccl_test.py \
-    $@
